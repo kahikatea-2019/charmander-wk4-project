7c951110b1fceb30e4a91a1e4b4e7cb4d11f9b15
Xjana Lamasi Luamanuvae-Su'a <xjana1234@gmail.com>
2019-04-12 10:50:17 +1200

    started nav

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,3 +1,20 @@
 import React from 'react'
 
+import book from '../../data/book'
+
+const chapterslist = object.keys(book)
+
+const Nav = () => {
+  return (
+    <div className='nav'>
+      <h2>Chapters</h2>
+      <ul>
+{rank}
+      </ul>
+
+    </div>
+
+  )
+}
+
 export default Nav
--- server/public/bundle.js
+++ server/public/bundle.js
@@ -102,7 +102,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 var App = function App() {
-  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Router"], null), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "React development has begun!"));
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "React development has begun!"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Router"], null));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (App);
