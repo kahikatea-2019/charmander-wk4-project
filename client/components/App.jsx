2285121c6c011f483372ea114ac793a6eea5b582
Matt Bialostocki <matt.bialostocki@gmail.com>
2019-04-11 17:26:48 +1200

    boilerplate

---
+++ client/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Router} from 'react-router-dom'
+const App = () => {
+  return (
+    <React.Fragment>
+    <Router/>
+    <h1>React development has begun!</h1>
+    <React.Fragment>
+  )
+}
+
+export default App
+
