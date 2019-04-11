a0a9933b736ed26e657dd1eb986fbb45d633e451
Matt Bialostocki <matt.bialostocki@gmail.com>
2019-04-11 17:37:26 +1200

    add data file bones

---
+++ data/book.js
@@ -0,0 +1,16 @@
+const coolBios = {
+  humans: [
+    { id: 1, chapterTitle: 'Lisa Liukas', imagePath: '' },
+    { id: 2, chapterTitle: 'Oliver Jeffers', imagePath: '' },
+    { id: 3, chapterTitle: 'Patti Smith', imagePath: '' },
+    { id: 4, chapterTitle: 'Marlon James', imagePath: '' },
+    { id: 5, chapterTitle: 'person who whistelblowed?', imagePath: '' }
+  ],
+  betterThanHumans: [
+    { id: 6, chapterTitle: 'Stitch', imagePath: '' },
+    { id: 7, chapterTitle: 'Minon 737', imagePath: '' },
+    { id: 8, chapterTitle: 'White Fang', imagePath: '' },
+    { id: 9, chapterTitle: 'Sorrow', imagePath: '' },
+    { id: 10, chapterTitle: 'Crazy Jane', imagePath: '' }
+  ]
+}
