b9348891a74a63dcc62dd71dde407e7d85f6c5d7
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-04-12 11:35:46 +1200

    route between home and coolbios xx

--- client/components/ContentDisplay.jsx
+++ client/components/ContentDisplay.jsx
@@ -1,3 +1,20 @@
 import React from 'react'
+import Nav from './Nav.jsx'
+import PageDisplay from './PageDisplay.jsx'
+
+class ContentDisplay extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1> wanna see a ccool book </h1>
+        <div>
+          <Nav />
+          <PageDisplay />
+        </div>
+      </React.Fragment>
+
+    )
+  }
+}
 
 export default ContentDisplay
