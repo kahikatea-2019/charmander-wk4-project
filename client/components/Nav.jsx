6b974c5abdc9849575a9879328ab39d1328c72f9
Paul Renwick <prenwick@gmail.com>
2019-04-12 13:29:45 +1200

    Displaying Nav Bar

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,23 +1,38 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-
+// import { Link } from 'react-router-dom'
 import book from '../../data/book'
 
-class Nav extends React.component {
-bookChapters = Object.keys(book).map(book => {
-  return <li key = {book}><Link to={`/index/${book`}>{book}</Link></li>
+class Nav extends React.Component {
+
+humanChapters = book.humans.map(item => {
+  return <li key={item.chapterTitle}>{item.chapterTitle}</li>
 })
+
+betterThanHumanChapters = book.betterThanHumans.map(item => {
+  return <li key={item.chapterTitle}>{item.chapterTitle}</li>
+})
+
 render () {
   return (
-    <div className='nav'>
-      <h2>Chapters</h2>
+    <div className="nav">
+      <h1>Nav</h1>
+      <h3>Humans</h3>
       <ul>
-        {this.bookChapters}
+        {this.humanChapters}
+      </ul>
+      <h3>Better Than Humans</h3>
+      <ul>
+        {this.betterThanHumanChapters}
       </ul>
     </div>
-
   )
 }
 }
 
 export default Nav
+
+// Want to map through the array 'humans' and the array
+// 'betterThanHumans' and then return the value for
+// chapterTitle
+
+// book.humans = [ {}, {}. {}, ]
