33536a6917e99bdd5b79710cfc284454f7f54237
Xjana Lamasi Luamanuvae-Su'a <xjana1234@gmail.com>
2019-04-12 15:24:01 +1200

    added descriptions

--- data/book.js
+++ data/book.js
@@ -1,11 +1,16 @@
 const book = {
   humans: [
-    { id: 1, chapterTitle: 'Lisa Liukas', imagePath: 'https://flockler.com/thumbs/sites/3187/linda-liukas-potretti-45dd9f59-d304-41d6-96f1-b6538cb2944f_s1260x0_q80_noupscale.jpg' },
-    { id: 2, chapterTitle: 'Oliver Jeffers', imagePath: 'https://www.digitalartsonline.co.uk/cmsdata/features/3669459/olive-jeffers-interview1.jpg' },
-    { id: 3, chapterTitle: 'Patti Smith', imagePath: 'http://bostonlitdistrict.org/wp-content/uploads/2017/08/Patti-Smith.jpg' },
-    { id: 4, chapterTitle: 'Marlon James', imagePath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuBmY-_xXrRAOZDFRLWyXb5zPvyx6r33Ghifpi9R3zVQof9ydq' },
-    { id: 5, chapterTitle: 'Ada Lovelace', imagePath: 'https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/ada_lovelace_portrait_by_chalon_1838.jpg?itok=4dzsoJbv&resize=1100x1100' }
+    { id: 1, chapterTitle: 'Lisa Liukas', imagePath: 'https://flockler.com/thumbs/sites/3187/linda-liukas-potretti-45dd9f59-d304-41d6-96f1-b6538cb2944f_s1260x0_q80_noupscale.jpg', description: 'Linda Liukas is the creator of Hello Ruby, a book that teaches children about the whimsical world of computers, technology and coding. Hello Ruby was the first children’s book that raised $380,000 on Kickstarter and is published in over 24 languages today.' },
+
+    { id: 2, chapterTitle: 'Oliver Jeffers', imagePath: 'https://www.digitalartsonline.co.uk/cmsdata/features/3669459/olive-jeffers-interview1.jpg', description: 'Oliver Jeffers (born 1977) is a Northern Irish artist, illustrator and writer who now lives and works in Brooklyn. He went to the integrated secondary school Hazelwood College, then graduated from the University of Ulster in 2001. From figurative painting and installation, to illustration and picture-book making, his work has been exhibited in New York, The Brooklyn Museum, Berlin, Dublin, London, Sydney, Washington, D.C., and Belfast.' },
+
+    { id: 3, chapterTitle: 'Patti Smith', imagePath: 'http://bostonlitdistrict.org/wp-content/uploads/2017/08/Patti-Smith.jpg', description: 'Patricia Lee Smith (born December 30, 1946) is an American singer-songwriter, poet, and visual artist who became an influential component of the New York City punk rock movement with her 1975 debut album Horses. On November 17, 2010, Smith won the National Book Award for her memoir Just Kids. The book fulfilled a promise she had made to her former long-time roommate and partner, Robert Mapplethorpe. She placed 47th in Rolling Stone magazines list of 100 Greatest Artists published in December 2010 and was also a recipient of the 2011 Polar Music Prize.' },
+
+    { id: 4, chapterTitle: 'Marlon James', imagePath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuBmY-_xXrRAOZDFRLWyXb5zPvyx6r33Ghifpi9R3zVQof9ydq', description: 'Marlon James (born 24 November 1970) is a Jamaican writer. He has published four novels: John Crows Devil (2005), The Book of Night Women (2009), A Brief History of Seven Killings (2014), winner of the 2015 Man Booker Prize, and Black Leopard, Red Wolf (2019). Now living in Minneapolis, James teaches literature at Macalester College in St. Paul, Minnesota. James is also a faculty lecturer at St. Francis Colleges Low Residency MFA in Creative Writing.' },
+
+    { id: 5, chapterTitle: 'Ada Lovelace', imagePath: 'https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/ada_lovelace_portrait_by_chalon_1838.jpg?itok=4dzsoJbv&resize=1100x1100', description: 'Augusta Ada King, Countess of Lovelace (née Byron; 10 December 1815 – 27 November 1852) was an English mathematician and writer, chiefly known for her work on Charles Babbages proposed mechanical general-purpose computer, the Analytical Engine. She was the first to recognise that the machine had applications beyond pure calculation, and published the first algorithm intended to be carried out by such a machine. As a result, she is sometimes regarded as the first to recognise the full potential of a "computing machine" and one of the first computer programmers.' }
   ],
+  
   betterThanHumans: [
     { id: 6, chapterTitle: 'Stitch', imagePath: 'https://vignette.wikia.nocookie.net/one-minute-meelee-fanon/images/7/7e/Stitch_626.png/revision/latest?cb=20160118210924' },
     { id: 7, chapterTitle: 'Minion 737', imagePath: 'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/88249/90861/Vacation-Minion-cardboard-cutout-buy-now-at-Starsills__17085.1497882763.jpg?c=2?imbypass=on' },
@@ -14,7 +19,5 @@ const book = {
     { id: 10, chapterTitle: 'Crazy Jane', imagePath: 'https://static.comicvine.com/uploads/scale_small/10/100647/6012194-jane.jpg' }
   ]
 }
-
-
 
 export default book
