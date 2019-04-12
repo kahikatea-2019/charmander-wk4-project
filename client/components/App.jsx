6c3f9c6fe4ba4273b6f3e558cb55fa9abf2b88ef
Paul Renwick <prenwick@gmail.com>
2019-04-12 16:09:21 +1200

    CSS

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import Nav from './Nav.jsx'
 import Home from './Home.jsx'
+import Title from './Title.jsx'
 import PageDisplay from './PageDisplay.jsx'
 
 class App extends React.Component {
@@ -9,18 +10,47 @@ class App extends React.Component {
     return (
       <Router>
         <React.Fragment>
+          <Route exact path= '/' component={Home} />
+
           <div className="content">
-            <div className="nav">
-              <Route path='/' component={Nav} />
+            <div className="title">
+              <Route path='/:author' component={Title} />
             </div>
-            <Route exact path= '/' component={Home} />
-            <div className="book">
-              <Route path='/:author' component={PageDisplay} />
+
+            <div className="container">
+              <div className="nav">
+                <Route path='/:author' component={Nav} />
+              </div>
+
+              <div className="book">
+                <Route path='/:author' component={PageDisplay} />
+              </div>
             </div>
+
           </div>
         </React.Fragment>
       </Router>
     )
   }
 }
+
+// class App extends React.Component {
+//   render () {
+//     return (
+//       <Router>
+//         <React.Fragment>
+//           <div className="content">
+//             <div className="nav">
+//               <Route path='/' component={Nav} />
+//             </div>
+//             <Route exact path= '/' component={Home} />
+//             <div className="book">
+//               <Route path='/:author' component={PageDisplay} />
+//             </div>
+//           </div>
+//         </React.Fragment>
+//       </Router>
+//     )
+//   }
+// }
 export default App
